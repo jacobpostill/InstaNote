12da1732aa0c90d6f850a246fc280d05680b1f53
Jacob Postill <postilljacob@gmail.com>
2023-02-16 17:06:28 -0500

    added routes

--- assets/paths/api.js
+++ assets/paths/api.js
@@ -1,10 +1,7 @@
-const db_data = require(".../db/db.json");
-module.exports = function(app) {
-    app.get("/api/notes/", function(req,res) {
-        res.json(db_data);
-    });
-    app.post("/api/notes/", function(req,res) {
-        db_data.push(req.body);
-        res.json(true);
-    })
-};
+const express = require('express');
+const expressRouter = require('./notesRoute');
+const app = express();
+
+app.use('/notesRoute', expressRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const indexPath = ('./assets/paths/api.js')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', indexPath);
 app.use(express.static("./public/"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'assets/html/notes.html'))
+);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'assets/html/notes.html'))
+);
+
+app.listen(PORT, () =>{
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
